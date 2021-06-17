81e85217da62f8e910e9a4215a16ac0afe9428ce
José Victor Alves <Victor-Siqueira@outlook.com.br>
2021-06-17 16:59:04 -0300

    Adicionando o projeto a o github

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device=width,user-scalable=0">
+	<title>HealthLife</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+<header>
+	<div class="container">
+		<img src="assets/images/logo.png">
+		<div class="menu">
+			<nav>
+				<ul>
+					<li class="selected"><a href="/Home">Home</a></li>
+					<li><a href="Contato/contato.html">Contatos</a></li>
+					<li><a href="/Home">Sobre</li>
+					<li><a href="/Home">Lista</li>
+					<li><a href="/Home">Nome</a></li>
+
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<section class="banner">
+	<div class="container column">
+		<div class="banner_headline">
+			<h1>Top notch experience.</h1>
+			<h3>HealthLife Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip.</h3>
+		</div>
+		<div class="banner_option">
+			<div class="banner1">
+				<h1>
+					Emergency Case
+				</h1>
+				<h4>if you need a doctor urgently outside of medicenter opening hours, call emergency appointmente number for emergency service.</h4>
+				<a href="">Read more</a>
+			</div>
+			<div class="banner2">
+				<h1>Doctors Timetable</h1>
+				<h4>Here at medicenter we have individual doctor's lists, click read more below to see services and current timestable for our doctor's</h4>
+
+				<a href="">Read more</a>
+			</div>
+			<div class="banner3">
+				<div class="open_hours">
+				<h1>Opening hours</h1>
+				<div class="banner-dias">Monday - Friday</div>
+				<div class="banner-horas">8:00 - 17:00</div>
+				<div class="banner-dias">Saturday</div>
+				<div class="banner-horas">9:30 - 17:30</div>
+				<div class="banner-dias">Sunday</div>
+				<div class="banner-horas">9:30 - 15:00</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+</body>
+</html>
+teste
