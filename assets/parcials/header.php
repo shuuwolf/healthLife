752899b306ca6ca9eea9cf5fc48d7956c29bfd92
José Victor Alves <Victor-Siqueira@outlook.com.br>
2021-06-21 14:55:45 -0300

    Modificando o banner

---
+++ assets/parcials/header.php
@@ -0,0 +1,14 @@
+<header>
+	<div class="container">
+		<div class="logo"></div><!-- LOGO -->
+		<nav class="menu">
+			<ul>
+				<li class="selected"><a href="/Home">Home</a></li>
+				<li><a href="Contato/contato.html">Contatos</a></li>
+				<li><a href="/Home">Sobre</li>
+				<li><a href="/Home">Lista</li>
+				<li><a href="/Home">Nome</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
--- home.php
+++ home.php
@@ -8,46 +8,31 @@
 </head>
 <body>
 
-<header>
-	<div class="container">
-		<div class="logo"></div><!-- LOGO -->
-		<nav class="menu">
-			<ul>
-				<li class="selected"><a href="/Home">Home</a></li>
-				<li><a href="Contato/contato.html">Contatos</a></li>
-				<li><a href="/Home">Sobre</li>
-				<li><a href="/Home">Lista</li>
-				<li><a href="/Home">Nome</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
+<?php require('./assets/parcials/header.php'); ?>
 
 <section class="banner">
 	<div class="container column">
-		<div class="banner_headline">
-			<h1>Top notch experience.</h1>
-			<h3>HealthLife Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip.</h3>
+		<div class="banner-title">
+			<h1>Curando suas efermidades!</h1>
+			<h3>HealthLife Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</h3>
 		</div>
 		<div class="banner_option">
 			<div class="banner1">
 				<h1>
-					Emergency Case
+					Exames
 				</h1>
 				<h4>if you need a doctor urgently outside of medicenter opening hours, call emergency appointmente number for emergency service.</h4>
 				<a href="">Read more</a>
 			</div>
 			<div class="banner2">
-				<h1>Doctors Timetable</h1>
+				<h1>Saiba sobre o doutor.</h1>
 				<h4>Here at medicenter we have individual doctor's lists, click read more below to see services and current timestable for our doctor's</h4>
 
 				<a href="">Read more</a>
 			</div>
 			<div class="banner3">
 				<div class="open_hours">
-				<h1>Opening hours</h1>
+				<h1>Horarios abertos!</h1>
 				<div class="banner-dias">Monday - Friday</div>
 				<div class="banner-horas">8:00 - 17:00</div>
 				<div class="banner-dias">Saturday</div>
@@ -56,6 +41,8 @@
 				<div class="banner-horas">9:30 - 15:00</div>
 			</div>
 			</div>
+			<div class="banner4">teste</div>
+			<div class="banner5">teste</div>
 		</div>
 	</div>
 </section>
